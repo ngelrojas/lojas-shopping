bf30c5b71eac95f889d7cdb7227e6e5c41d2718e
ngel rojas p <aomarrojasp@gmail.com>
2020-05-07 23:31:54 -0300

    uncomment scripts to mode development

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -11,9 +11,9 @@ then
     echo "POSTGESQL STARTED"
 fi
 
-#python manage.py flush --no-input
-#python manage.py makemigrations
-#python manage.py migrate
-#python manage.py collectstatic --no-input --clear
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --no-input --clear
 
 exec "$@"
